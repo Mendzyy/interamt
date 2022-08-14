800b170b162dcf0edad527ea1d5ae4367dffd632
Mendzyy <chiragmendapara.cm@gmail.com>
2022-08-14 22:46:59 +0200

    Modal Page as Component  - CM

--- interamt/src/app/folder/folder.page.ts
+++ interamt/src/app/folder/folder.page.ts
@@ -1,32 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { ModalController } from '@ionic/angular';
+import { DialogPage } from '../dialog/dialog.page';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+  public page: string;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private _aR: ActivatedRoute, private _modal: ModalController) {}
 
   ngOnInit() {
-    this.folder =
-      this.activatedRoute.snapshot.paramMap.get('id') === 'countries'
+    // Fetching ID from the Menu
+    this.page =
+      this._aR.snapshot.paramMap.get('id') === 'countries'
         ? 'Länder'
         : 'Projekte';
   }
 
-  addItem()
-  {
-    if(this.folder && this.folder === "Länder")
-    {
-      
-    }
-    else
-    {
-
-    }
+  async addItem() {
+    await this._modal
+      .create({
+        component: DialogPage,
+        componentProps: {
+          page: this.page,
+        },
+      })
+      .then((_oModal) => _oModal.present());
   }
 }
