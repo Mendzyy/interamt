34b92b53e7a24cf20eb055f850f48362f4b8de68
Mendzyy <chiragmendapara.cm@gmail.com>
2022-08-14 22:46:29 +0200

    Dialog Page Routing - CM

--- interamt/src/app/app-routing.module.ts
+++ interamt/src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'dialog',
+    loadChildren: () => import('./dialog/dialog.module').then( m => m.DialogPageModule)
   }
 ];
 
