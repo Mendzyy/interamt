b978e99ac97afa71eb488caee20e48649aec423b
Mendzyy <chiragmendapara.cm@gmail.com>
2022-08-14 22:46:01 +0200

    FB Config - CM

--- interamt/src/environments/environment.prod.ts
+++ interamt/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCBIRaqx3xilWynpDcSbmNfo01rwJPtfxQ',
+    authDomain: 'interamt-test.firebaseapp.com',
+    projectId: 'interamt-test',
+    storageBucket: 'interamt-test.appspot.com',
+    messagingSenderId: '397460190162',
+    appId: '1:397460190162:web:b47d1dcecaadc8d0b1ce0b',
+  },
 };
